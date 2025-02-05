b7895c53ab17cba91ed1b8287ef46c52baafa19d
zakius <2071503+zakius@users.noreply.github.com>
2025-02-05 08:51:02 +0100

    fix: formatting

--- src/lib/cacheable-axios.ts
+++ src/lib/cacheable-axios.ts
@@ -44,50 +44,60 @@ export const buildAxios = (redis: Redis) => {
     }),
   });
 
-  axiosInstance.interceptors.request.use(async (config) => {
-    if (!config.url) {
-      return config;
-    }
-    try {
-      const url = new URL(config.url);
-      if (browserUasOrigins.includes(url.origin)) {
-        config.headers['User-Agent'] = browserUserAgent; // Set User-Agent
-      } else {
-        config.headers['User-Agent'] = `SmartRSS/FeedFathom ${cachedBuildTimestamp}`;
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      if (!config.url) {
+        return config;
       }
-
-      return config;
-    } catch {
-      return config;
-    }
-  }, async (error) => {
-    const config = error.config;
-    const originalUserAgent = config.headers['User-Agent'];
-
-    // Check if the request was not already using the browser User-Agent
-    if (error.code === 'ECONNREFUSED' && originalUserAgent !== browserUserAgent && config.__retryCount < MAX_RETRIES) {
-      config.__retryCount = config.__retryCount || 0;
-      config.__retryCount += 1;
-
-      await Bun.sleep(2000); // Wait for 2 seconds before retrying
-      config.headers['User-Agent'] = browserUserAgent; // Set to browser User-Agent
-
-      const response = await axiosInstance.request(config); // Retry the request
-
-      // Check the response status
-      if (response.status >= 200 && response.status < 400) {
-        // Update the browserUasOrigins array if the request succeeds
+      try {
         const url = new URL(config.url);
-        if (!browserUasOrigins.includes(url.origin)) {
-          browserUasOrigins.push(url.origin);
+        if (browserUasOrigins.includes(url.origin)) {
+          config.headers["User-Agent"] = browserUserAgent; // Set User-Agent
+        } else {
+          config.headers["User-Agent"] =
+            `SmartRSS/FeedFathom ${cachedBuildTimestamp}`;
         }
-        return response; // Return the successful response
-      } else {
-        return Promise.reject(new Error(`Request failed with status ${response.status}`));
+
+        return config;
+      } catch {
+        return config;
       }
-    }
-    return Promise.reject(error);
-  });
+    },
+    async (error) => {
+      const config = error.config;
+      const originalUserAgent = config.headers["User-Agent"];
+
+      // Check if the request was not already using the browser User-Agent
+      if (
+        error.code === "ECONNREFUSED" &&
+        originalUserAgent !== browserUserAgent &&
+        config.__retryCount < MAX_RETRIES
+      ) {
+        config.__retryCount = config.__retryCount || 0;
+        config.__retryCount += 1;
+
+        await Bun.sleep(2000); // Wait for 2 seconds before retrying
+        config.headers["User-Agent"] = browserUserAgent; // Set to browser User-Agent
+
+        const response = await axiosInstance.request(config); // Retry the request
+
+        // Check the response status
+        if (response.status >= 200 && response.status < 400) {
+          // Update the browserUasOrigins array if the request succeeds
+          const url = new URL(config.url);
+          if (!browserUasOrigins.includes(url.origin)) {
+            browserUasOrigins.push(url.origin);
+          }
+          return response; // Return the successful response
+        } else {
+          return Promise.reject(
+            new Error(`Request failed with status ${response.status}`),
+          );
+        }
+      }
+      return Promise.reject(error);
+    },
+  );
 
   const redisStorage = buildStorage({
     async find(key) {
