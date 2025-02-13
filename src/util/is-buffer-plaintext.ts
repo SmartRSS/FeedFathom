cc23be42e651ca563a07fd5e1117ff779adcd72e
zakius <2071503+zakius@users.noreply.github.com>
2025-02-13 18:32:54 +0100

    Improve favicon rendering and storage with data URI support

---
+++ src/util/is-buffer-plaintext.ts
@@ -0,0 +1,14 @@
+export function isBufferPlaintext(buffer: Buffer) {
+  // Check if the buffer contains only printable characters
+  for (let i = 0; i < buffer.length; i++) {
+    const byte = buffer[i];
+    if (!byte) {
+      return false;
+    }
+    // Check if the byte is outside the range of printable ASCII characters
+    if (byte < 32 && byte !== 9 && byte !== 10 && byte !== 13) {
+      return false; // Found a non-printable character
+    }
+  }
+  return true; // All characters are printable
+}
