a17a4f63b2ee49961a91ffefefbbb507dc3b5ee5
zakius <2071503+zakius@users.noreply.github.com>
2025-04-02 19:58:07 +0200

    hc 1

--- src/routes/healthcheck/+server.ts
+++ src/routes/healthcheck/+server.ts
@@ -1,5 +1,5 @@
-import { json } from "@sveltejs/kit";
+import { type RequestHandler, json } from "@sveltejs/kit";
 
-export const GET = () => {
-  return json({ status: "healthy" }, { status: 200 });
+export const GET: RequestHandler = () => {
+  return json({ status: "ok" });
 };
