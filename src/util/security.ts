53cc7e8d18c9cae4aaa611a3b550e7d65bb67f5a
zakius <git config --global user.name Your>
2025-04-29 21:47:38 +0200

    upd

--- src/util/security.ts
+++ src/util/security.ts
@@ -8,25 +8,25 @@ interface RequestWithHeaders {
  * Works with both SvelteKit RequestEvent and Bun Request
  */
 export const isInternalRequest = <T extends RequestWithHeaders>(
-  req: T,
+  _req: T,
 ): boolean => {
   return true;
   // TODO
-  const forwardedFor = req.headers.get("x-forwarded-for");
-  const realIp = req.headers.get("x-real-ip");
+  //   const forwardedFor = req.headers.get("x-forwarded-for");
+  //   const realIp = req.headers.get("x-real-ip");
 
-  // If there are any forwarded headers, request is coming through a proxy
-  if (forwardedFor || realIp) {
-    return false;
-  }
+  //   // If there are any forwarded headers, request is coming through a proxy
+  //   if (forwardedFor || realIp) {
+  //     return false;
+  //   }
 
-  // Try to get IP using available methods
+  //   // Try to get IP using available methods
 
-  if (req.address) {
-    return req.address === "127.0.0.1" || req.address === "::1";
-  }
+  //   if (req.address) {
+  //     return req.address === "127.0.0.1" || req.address === "::1";
+  //   }
 
-  // For requests without IP check capability, we can only check headers
-  // Note: This is less secure than the version with IP check
-  return true;
+  //   // For requests without IP check capability, we can only check headers
+  //   // Note: This is less secure than the version with IP check
+  //   return true;
 };
