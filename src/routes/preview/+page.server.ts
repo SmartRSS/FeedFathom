b7fa306c8fdf1e379cd77b72f4d0238ccbe33e7d
zakius <git config --global user.name Your>
2025-05-04 19:24:05 +0200

    fix

--- src/routes/preview/+page.server.ts
+++ src/routes/preview/+page.server.ts
@@ -1,5 +1,5 @@
 import { getMailFeatureState } from "../../util/is-mail-enabled.ts";
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad } from "./$types.ts";
 
 export const load: PageServerLoad = ({ locals }) => {
   const { appConfig } = locals.dependencies;
